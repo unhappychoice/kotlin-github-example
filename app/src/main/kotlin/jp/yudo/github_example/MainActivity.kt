eb40619f9a569187cb89c417051ea5fffd5e1613
ueki <ueki@yudo.jp>
2015-03-22 23:43:37 +0900

    first commit

---
+++ app/src/main/kotlin/jp/yudo/github_example/MainActivity.kt
@@ -0,0 +1,13 @@
+package jp.yudo.github_example
+
+import android.os.Bundle
+import android.app.Activity
+import android.view.Menu
+import android.view.MenuItem
+
+public class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
